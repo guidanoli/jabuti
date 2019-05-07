abdb1ad6d06c2063588f3945f49d8b4943e06078
Guilherme Dantas <guidanoli@hotmail.com>
2019-05-07 09:56:48 -0300

    Added Eclipse project with basic GUI classes

---
+++ src/gui/MainFrame.java
@@ -0,0 +1,57 @@
+package gui;
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame {
+
+	// size parameters
+	public final int DEF_H = 600;
+	public final int DEF_W = 800;
+	
+	// containers
+	JPanel p = new MainPanel();
+	
+	// components
+	
+	public MainFrame(String name) {
+		// set frame name
+		super(name);
+		// set bounds in the middle
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension d = tk.getScreenSize();
+		int s_w = d.width;
+		int s_h = d.height;
+		int x = (s_w - DEF_W)/2;
+		int y = (s_h - DEF_H)/2;
+		setBounds(x,y,DEF_W,DEF_H);
+		// exit on close
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		// set panel layout
+		getContentPane().add(p);
+		// set look and feel
+		try {
+			// windows :)
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		}
+		catch (ClassNotFoundException e)
+		{
+			System.out.println("Class not found.");
+		}
+		catch (UnsupportedLookAndFeelException e)
+		{
+			System.out.println("Unsupported look & feel.");
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static void main(String[] args) {
+		MainFrame painel = new MainFrame("SetMeUp v.1.0");
+		painel.setVisible(true);
+		//code
+	}
+	
+}
---
+++ src/gui/MainPanel.java
@@ -0,0 +1,16 @@
+package gui;
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class MainPanel extends JPanel {
+	
+	public MainPanel(){
+		// constructor
+		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		setBackground(Color.WHITE);
+	}
+	
+	// nothing much yet...
+	
+}
