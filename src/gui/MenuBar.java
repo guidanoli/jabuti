cb58fa0e3ee550744bb52d280f2f8f04920c72fe
Guilherme Dantas <guidanoli@hotmail.com>
2019-05-07 10:19:16 -0300

    Menu bar improvements

--- src/gui/MenuBar.java
+++ src/gui/MenuBar.java
@@ -1,16 +1,19 @@
 package gui;
+import java.awt.event.*;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class MenuBar extends JMenuBar {
 
-	private JMenu file_menu = new JMenu("File");
 	private JMenu edit_menu = new JMenu("Edit");
 	private JMenu about_menu = new JMenu("About");
 		
 	public MenuBar() {
-		add(file_menu);
+		edit_menu.setMnemonic(KeyEvent.VK_E);
+		edit_menu.add(new JMenuItem("Preferences...",KeyEvent.VK_P));
 		add(edit_menu);
+		about_menu.setMnemonic(KeyEvent.VK_A);
+		about_menu.add(new JMenuItem("About SetMeUp",KeyEvent.VK_S));
 		add(about_menu);
 		// constructor
 	}
