a8d674af100f15b3c2963b5e6f7d39996133b769
Guilherme Dantas <guidanoli@hotmail.com>
2019-05-30 10:23:23 -0300

    Corrected resources folder name + icons + LocalRessources class updated

--- src/vars/LocalRessources.java
+++ src/vars/LocalRessources.java
@@ -2,12 +2,30 @@ package vars;
 
 public class LocalRessources {
 
-	public static final String ressourcefolder = "ressources";
+	/* *******
+	 * FOLDERS
+	 * ******* */
+	public static final String ressourcefolder = "resources";
 	public static final String configfolder = ressourcefolder+"/config";
 	public static final String langfolder = ressourcefolder+"/lang";
 	public static final String imgsfolder = ressourcefolder+"/imgs";
+	
+	/* ***
+	 * XML
+	 * *** */
 	public static final String properties = configfolder+"/properties.xml";
 	public static final String metalang = langfolder+"/_meta_.xml";
+	
+	/* *****
+	 * ICONS
+	 * ***** */
 	public static final String icon = imgsfolder+"/setmeup.png";
+	public static final String success = imgsfolder+"/success.png";
+	public static final String error = imgsfolder+"/error.png";
+	public static final String warning = imgsfolder+"/warning.png";
+	public static final String minus = imgsfolder+"/minus.png";
+	public static final String pause = imgsfolder+"/pause-1.png";
+	public static final String stop = imgsfolder+"/stop-1.png";
+	public static final String info = imgsfolder+"/info.png";
 	
 }
