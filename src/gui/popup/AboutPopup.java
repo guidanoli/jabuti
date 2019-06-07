bf201d3b4de7f820536228b802dcc47b72dc5017
Guilherme Dantas <guidanoli@hotmail.com>
2019-06-07 14:16:29 -0300

    AboutPopup tided up and now a modal

--- src/gui/popup/AboutPopup.java
+++ src/gui/popup/AboutPopup.java
@@ -17,67 +17,133 @@ import gui.error.FatalError;
 import gui.maindlg.MenuPopup;
 import vars.Language;
 
-
+/**
+ * The 'About' dialog shows information about software itself and
+ * the ones that made it possible.
+ * @author guidanoli
+ *
+ */
 public class AboutPopup implements MenuPopup {
 	
 	private Language lang = Language.getInstance();
-	private JFrame frame = new JFrame(lang.get("gui_popup_about_title"));
-	private final int margin = 20;
-	private final int img_size = 50;
-	String [] lines = {	"<div align='center'>" +
-			lang.get("name") + " " + lang.get("version"),
-			lang.get("authors"),
-			getRepositoryLinkHTML("GitHub Repository"),
-			"<br><hr><br>" + getIconsAuthorHTML() + "</div>" };
 	
+	private JDialog dlg;
+	private JFrame parent;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void open(JFrame parent) {
+		dlg.pack();
+		dlg.setResizable(false);
+		dlg.setLocationRelativeTo(parent);
+		dlg.setVisible(true);
+	}
+	
+	/**
+	 * Constructs About Pop Up but does not open
+	 * @param parent - parent frame
+	 */
 	public AboutPopup(JFrame parent) {
-		GridBagLayout layout = new GridBagLayout();
+		this.parent = parent;
+		dlg = new JDialog(parent,lang.get("gui_popup_about_title"),true);
+		JPanel panel = new JPanel(new GridBagLayout());
+		buildDialog(panel);
+		panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+		dlg.getContentPane().add(panel);
+	}
+
+	private void buildDialog(JPanel panel) {
+		JLabel iconLabel = buildIcon();
+		JEditorPane jep = buildHyperText();
 		GridBagConstraints c = new GridBagConstraints();
-		JPanel panel = new JPanel(layout);
+		
+		/*
+		 * Icon Label
+		 * **********/
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 0;
+		c.gridy = 0;
+		panel.add(iconLabel, c);
+		
+		/*
+		 * Hyper Text
+		 * **********/
+		c.gridy = 1;
+		c.insets = new Insets(10,0,0,0);
+		panel.add(jep, c);
+	}
+	
+	/**
+	 * @return Software Icon Component
+	 */
+	private JLabel buildIcon() {
 		BufferedImage myPicture = null;
-		try {
-			myPicture = ImageIO.read(new File(vars.LocalResources.icon));
-		} catch (IOException e) {
-			FatalError.show(e,parent,false);
-		}
+		try { myPicture = ImageIO.read(new File(vars.LocalResources.icon)); }
+		catch (IOException e) { FatalError.show(e,parent,false); }
 		JLabel picLabel;
-		if( myPicture != null )
-			picLabel = new JLabel(new ImageIcon(myPicture.getScaledInstance(img_size, img_size, Image.SCALE_FAST)));
-		else
-			picLabel = new JLabel("<Missing image>");
+		if( myPicture != null ) picLabel = new JLabel(new ImageIcon(myPicture.getScaledInstance(50, 50, Image.SCALE_FAST)));
+		else picLabel = new JLabel("<Missing image>");
+		return picLabel;
+	}
+	
+	/**
+	 * @return HTML Component
+	 */
+	private JEditorPane buildHyperText() {
 		JEditorPane jep = new JEditorPane();
 	    jep.setContentType("text/html");
 	    jep.setText(getHyperText());
 	    jep.setEditable(false);
 	    jep.setOpaque(false);
-	    jep.addHyperlinkListener( e -> {
+	    jep.addHyperlinkListener(e -> {
 		    if (HyperlinkEvent.EventType.ACTIVATED.equals(e.getEventType())) {
 		    	Desktop desktop = Desktop.getDesktop();
 		        try {
 		        	desktop.browse(e.getURL().toURI());
 		        } catch (Exception ex) {
-		        	FatalError.show(ex);
+		        	FatalError.show(ex,parent);
 		        }
 		    }
 	    });
-		c.fill = GridBagConstraints.BOTH;
-		c.gridx = 0;
-		c.gridy = 0;
-		panel.add(picLabel, c);
-		c.gridy = 1;
-		c.insets = new Insets(10,0,0,0);
-		panel.add(jep, c);
-		panel.setBorder(BorderFactory.createEmptyBorder(margin, margin, margin, margin));
-		frame.getContentPane().add(panel);
-		frame.setResizable(false);
-		gui.DefaultFrame.set(frame);
+	    return jep;
 	}
-
-	public void open(JFrame parent) { frame.setVisible(true); }
-	protected String getHyperText() { return String.join("<br>", lines); }
-	protected String getRepositoryLinkHTML(String label) {
+		
+	/**
+	 * Joins all HTML lines to one single HTML text
+	 * @return HTML
+	 */
+	private String getHyperText() {
+		
+		String header = String.format("%s %s", lang.get("name"), lang.get("version"));
+		String authors = lang.get("authors");
+		String repolink = getRepositoryLinkHTML("GitHub Repository");
+		String iconscredits = getIconsAuthorHTML(); 
+		
+		String [] lines = {
+			header,
+			authors,
+			repolink,
+			"",
+			iconscredits
+		};
+		
+		String joinedLines = String.join("<br>", lines); 
+		
+		return String.format("<div align='center'>%s</div>", joinedLines);
+	}
+	
+	/**
+	 * @param label - name of repository link label
+	 * @return HTML of hyperlink to repository
+	 */
+	private String getRepositoryLinkHTML(String label) {
 		return String.format("<a href=\"%s\">%s</a>", lang.get("repository"), label);
 	}
+	
+	/**
+	 * @return HTML of credits to icons authors
+	 */
 	private String getIconsAuthorHTML() {
 		String [] authorsName = {"Smashicons","Double-J Design"};
 		String [] authorsURL = {"https:/www.flaticon.com/authors/smashicons/","http://www.doublejdesign.co.uk/"};
