33bc6b24d930465ecd64ca88093e52bdf1e75e43
guidanoli <guidanoli@morris.tecgraf.puc-rio.br>
2019-06-18 11:18:42 -0300

    Improved lang.py

--- resources/lang/lang.py
+++ resources/lang/lang.py
@@ -36,14 +36,21 @@ def found_in_file(key, file):
 """ add new_key inputted by stdin in every file that doesn't have it """
 
 
-def new_key(files_list):
-  new_key = input("New key (or 'end'): ")
+def new_key(files_list,saved_key):
+  if len(saved_key) > 0 :
+    is_using_saved_keyname = "y" in input("Would you like to use key '"+saved_key+"' (y/n)? ").lower()
+  else:
+    is_using_saved_keyname = False
+  if not is_using_saved_keyname:
+    new_key = input("New key (or 'end'): ")
+  else:
+    new_key = saved_key
   if new_key.strip().upper() == "END":
-    return False, False
+    return False, False, False
   if len(new_key) == 0:
-    return False, "Too short"
+    return False, "Too short", False
   if invalid_xml(new_key):
-    return False, "Invalid character found"
+    return False, "Invalid character found", False
   found_in = {}
   for file in files_list:
     found_in[file] = found_in_file(new_key, file)
@@ -55,11 +62,13 @@ def new_key(files_list):
           lacking.append(k)
       print(new_key+" is missing in files "+", ".join(lacking))
       print(new_key+" will apply only to the missing files")
-      return set_new_key(new_key, lacking)
+      set_new_key(new_key, lacking)
+      return True, "", is_using_saved_keyname
     else:
-      return False, new_key+" already exists in all files"
+      return False, new_key+" already exists in all files", is_using_saved_keyname
   else:
-    return set_new_key(new_key, files_list)
+    set_new_key(new_key, files_list)
+    return True, "", is_using_saved_keyname
 
 
 """ returns new content with new key and value """
@@ -116,7 +125,6 @@ def set_new_key(key, files_list):
       content = add_key_on_content(content, key, value)
     with open(file, 'w', encoding='utf-8') as f:
       f.write(content)
-  return True, ""
 
 
 """ Returns header STRING """
@@ -174,11 +182,115 @@ def sort_keys(files_list):
       entries.sort(key=operator.itemgetter(1))  # sort
       content = format_xml(header, entries, footer)
     with open(file, 'w', encoding='utf-8') as f:
-      f.write(content)
+      f.write(content)  
 
-success, error = new_key(files)
-while(success):
-  success, error = new_key(files)
-  sort_keys(files)
-if error:
-  print(error)
+def add_key(files_list,saved_key):
+  success, error, used_key = new_key(files_list, saved_key)
+  while success:
+    if used_key:
+      saved_key = ""
+    sort_keys(files_list)
+    success, error, used_key = new_key(files_list,saved_key)
+  if error:
+    print(error)
+  return used_key
+
+""" get keys from language file as an array of strings """
+
+def get_keys_from_langfile(file):
+  with open(file, 'r', encoding='utf-8') as f:
+    content = f.read()
+  entries = get_entries(content)
+  keys = []
+  for e in entries:
+    keys.append(e[1])
+  return keys
+
+""" display key tree on screen """
+""" returns key navigated by the explorer """
+
+def show_key_tree(files_list):
+  keys = []
+  for file in files_list:
+    new_keys = get_keys_from_langfile(file)
+    for k in new_keys:
+      if not k in keys:
+        keys.append(k)
+  tree = dict()
+  for k in keys:
+    parts = k.split("_")
+    node = tree
+    for p in parts:
+      if not p in node:
+        node[p] = dict()
+      node = node[p]
+  depth = int(input("depth (-1 = show all tree): "))
+  print_tree_root(tree, depth)
+  new_node = input("\nnode: ").lower()
+  key_parts = [ new_node ]
+  while new_node in tree:
+    tree = tree[new_node]
+    print("==================================")
+    print_tree_root(tree, depth)
+    new_node = input("\nnode: ").lower()
+    key_parts.append(new_node)
+  while new_node != "$":
+    new_node = input("\nnode: ").lower()
+    if new_node != "$":
+      key_parts.append(new_node)
+  key = "_".join(key_parts)
+  print("key: "+key)
+  return key
+
+""" prints tree from root with defined recursion depth """
+
+def print_tree_root(tree,depth):
+  print_tree(tree, 0, "",depth)
+
+""" prints tree to screen, identing according to node height """
+""" tree - dictionary tree """
+""" height - current height """
+""" ident_chars - characters at the left of node """
+""" depth - recursion depth """
+
+def print_tree(tree,height,ident_chars,depth):
+  if depth == 0:
+    return
+  count = len(tree)
+  i = 0
+  last_node = "\u2514"
+  middle_node = "\u251c"
+  parent_node = "\u2502"
+  if count > 0:
+    print(ident_chars+parent_node)
+  for node_name, node_dict in tree.items():
+    if i == count - 1:
+      symbol = last_node
+      parent_symbol = " "
+    else:
+      symbol = middle_node
+      parent_symbol = parent_node
+    if len(node_dict) > 0:
+      node_name = "[" + node_name + "]"
+    print(ident_chars+symbol+node_name)
+    print_tree(node_dict,height+1,ident_chars+parent_symbol,depth-1)
+    i += 1
+
+def show_diff(files_list):
+  print("wip")
+
+cmd_msg = "(a)dd/(t)ree/(d)iff/(e)nd: "
+cmd = input(cmd_msg).lower()
+saved_key = ""
+while not 'e' in cmd:
+  if 'a' in cmd:
+    used_key = add_key(files, saved_key)
+    if used_key:
+      saved_key = ""
+  elif 't' in cmd:
+    saved_key = show_key_tree(files)
+  elif 'd' in cmd:
+    show_diff(files)
+  else:
+    print(cmd+" not recognized")
+  cmd = input('\n'+cmd_msg).lower()
